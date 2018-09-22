0dca9901d747eb22287fbab2e195d04635ec4544
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-09-22 11:53:50 +0100

    added artist model and controller code

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
+const Artist = require('./controllers/artist');
+
 
 require('dotenv').config({
   path: path.join(__dirname, './settings.env'),
@@ -9,7 +11,9 @@ require('dotenv').config({
 
 const app = express();
 mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true });
+
 app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello MongoDb!'));
+app.post('/artist', Artist.post);
 
 app.listen(3000, () => console.log('It works!'));
---
+++ controllers/artist.js
@@ -0,0 +1,8 @@
+const Artist = require('../models/artist');
+
+exports.post = (req, res) => {
+  const artist = new Artist({ name: req.body.name, genre: req.body.genre });
+  artist.save((err, artistCreated) => {
+    res.json(artistCreated);
+  });
+};
