0d500bfc83393ed79bf6df7e8814a65aa2327e02
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-09-22 20:52:08 +0100

    added createArtist, getArtist & getListArtist tests.

---
+++ __tests__/controllers/getArtist.test.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const path = require('path');
+const httpMocks = require('node-mocks-http');
+const events = require('events');
+const { get } = require('../../controllers/artist');
+const Artist = require('../../models/artist');
+
+require('dotenv').config({
+  path: path.join(__dirname, '../../settings.env'),
+});
+
+describe('Artist GET Endpoint', () => {
+  beforeAll((done) => {
+    mongoose.connect(process.env.TEST_DATABASE_CONN, done);
+  });
+  
+  it('should retrieve Artist record from database', (done) => {
+    const artist = new Artist({ name: 'Wu-Tang Clan', genre: 'HipHop' });
+    artist.save((err, artistCreated) => {
+      if (err) {
+        console.log(err, 'something went wrong');
+      }
+      const request = httpMocks.createRequest({
+        method: 'GET',
+        URL: '/Artist',
+        params: {
+          artistId: artistCreated._id,
+        }
+      });
+      const response = httpMocks.createResponse({
+        eventEmitter: events.EventEmitter,
+      });
+
+      get(request, response);
+
+      response.on('end', () => {
+        let artistFound = JSON.parse(response._getData());
+        expect(artistFound.name).toBe('Wu-Tang Clan');
+        expect(artistFound.genre).toBe('HipHop');
+        done();
+      });
+    });
+  });
+  afterEach((done) => {
+    Artist.collection.drop((e) => {
+      if (e) {
+        console.log(e);
+      }
+      done();
+    });
+  });
+  afterAll(() => {
+    mongoose.connection.close();
+  });
+});
---
+++ __tests__/controllers/getListArtist.test.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const path = require('path');
+const httpMocks = require('node-mocks-http');
+const events = require('events');
+const { list } = require('../../controllers/artist');
+const Artist = require('../../models/artist');
+
+require('dotenv').config({
+  path: path.join(__dirname, '../../settings.env'),
+});
+
+describe('Artist List GET Endpoint', () => {
+    beforeAll((done) => {
+      mongoose.connect(process.env.TEST_DATABASE_CONN, done);
+    });
+
+    it('should retrieve an Artist List from the database', (done) => {
+        const artists = [
+            { name: 'tame impala', genre: 'rock' },
+            { name: 'jamal', genre: '90s hiphop' },
+            { name: 'Jeru the Damaja', genre: '90s hiphop' },
+          ];
+          Artist.create(artists, (err) => {
+            if (err) {
+              console.log(err, 'stuff went wrong');
+            }
+            const request = httpMocks.createRequest({
+              method: 'GET',
+              URL: '/Artist/',
+            });
+            const response = httpMocks.createResponse({
+              eventEmitter: events.EventEmitter,
+            });
+            list(request, response);
+            response.on('end', () => {
+              const listOfArtists = JSON.parse(response._getData());
+              console.log(listOfArtists);
+              const artistsNames = listOfArtists.map(e => e.name);
+              console.log(artistsNames);
+              expect(artistsNames).toEqual(expect.arrayContaining(
+                [
+                  'tame impala', 
+                  'jamal', 
+                  'Jeru the Damaja',
+                ]
+                ));
+              expect(artistsNames).toHaveLength(3);
+              done();
+            });
+        });
+    });
+    afterEach((done) => {
+      Artist.collection.drop((e) => {
+        if (e) {
+          console.log(e);
+        }
+        done();
+      });
+    });
+    afterAll(() => {
+      mongoose.connection.close();
+    });
+});
